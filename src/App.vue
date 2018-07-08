297527ab09cdf088d3254fbd8b432dfad70f16d7
devmount <mail@devmount.de>
2018-07-08 10:45:19 +0200

    added: bootstrap, adjusted example app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <b-btn @click="addDay" variant="outline-info" class="m-1"><font-awesome-icon icon="plus" /></b-btn>
+    </div>
+    <ul class="dayList">
+      <li v-for="(day, index) in days" :key="index">
+        <b-alert :variant="states[day.status]" show>{{ day.name }}</b-alert>
+        <b-btn @click="updateDay(day, 1)" variant="success" class="m-0"><font-awesome-icon icon="circle" /></b-btn>
+        <b-btn @click="updateDay(day, 0)" variant="default" class="m-0"><font-awesome-icon icon="circle" /></b-btn>
+        <b-btn @click="updateDay(day, -1)" variant="danger" class="m-0"><font-awesome-icon icon="circle" /></b-btn>
+        <b-btn @click="deleteDay(day)" variant="danger" class="ml-2"><font-awesome-icon icon="times" /></b-btn>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { db } from './firebase'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    data() {
+      return {
+        days: [],
+        states: {1: 'success', 0: 'default', '-1': 'danger'}
+      }
+    },
+    firestore() {
+      return {
+        days: db.collection('days'),
+      }
+    },
+    methods: {
+      addDay: function() {
+        this.$firestore.days.add(
+          {
+            name: '2018-07-07',
+            status: 0,
+            timestamp: new Date()
+          }
+        )
+      },
+      updateDay: function(day, status) {
+        this.$firestore.days.doc(day['.key']).update({status: status})
+      },
+      deleteDay: function(day) {
+        this.$firestore.days.doc(day['.key']).delete()
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .dayList {
+    list-style: none;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+// Vue Js
 import Vue from 'vue'
 import App from './App.vue'
+// Firestore
+import VueFirestore from 'vue-firestore'
+Vue.use(VueFirestore)
+// Bootstrap
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimes, faPlus, faCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faTimes, faPlus, faCircle)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
+// turns off the 'You are running Vue in development mode.' msg
 Vue.config.productionTip = false
 
 new Vue({
