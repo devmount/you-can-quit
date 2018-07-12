54a3bf8ff66b3d48b80419ac2ca3554d77ef9dc9
Andreas Müller <mail@devmount.de>
2018-07-12 18:29:05 +0200

    created: stats component

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,15 +1,18 @@
 <template>
 <div class="info">
   <Legend />
+  <Stats />
 </div>
 </template>
 
 <script>
 import Legend from './Legend.vue'
+import Stats from './Stats.vue'
 
 export default {
   components: {
-    Legend
+    Legend,
+    Stats
   },
   props: {}
 }
---
+++ src/components/Stats.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="container">
+  <h2>Stats</h2>
+  <div class="stats">
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
