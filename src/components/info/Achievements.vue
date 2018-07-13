b4e4908ce308a8694412078daecf7a0045c4fc73
Andreas Müller <mail@devmount.de>
2018-07-13 23:28:12 +0200

    added: achievements section

---
+++ src/components/info/Achievements.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="container">
+  <h2>Achievements</h2>
+  <div class="achievements">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    statusData: Object,
+  },
+  methods: {
+    // build date format yyyy-mm-dd
+    getDate (year, month, day) {
+      return year + '-' + ('0' + month).slice(-2) + '-' + ('0' + day).slice(-2)
+    },
+  },
+  computed: {
+    // get the minimum date (edited date that is most past)
+    minDate () {
+      var keys = Object.keys(this.statusData)
+      if (typeof keys !== 'undefined' && keys.length > 0) {
+        return keys.reduce(function (p, v) {
+          var pd = new Date(p), vd = new Date(v)
+          return ( pd < vd ? pd : vd );
+        });
+      }
+    },
+  }
+}
+</script>
+
+<style>
+.stats {
+  display: flex;
+  flex-flow: row wrap;
+  cursor: default;
+}
+.stats .item {
+  width: 33%;
+}
+</style>
