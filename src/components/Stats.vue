f322f1ad637bb8bdb1b3946c02ec42d4bdebf07b
Andreas Müller <mail@devmount.de>
2018-07-13 10:37:51 +0200

    improved: current streak calculation

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -22,11 +22,12 @@ export default {
   computed: {
     // get number of successful days in a row preceding today
     currentStreak: function() {
-      var streak = 0, n = new Date(), success = true
+      var streak = 0, n = new Date(), success = true, key = ''
       while (success) {
         n = new Date(n.setDate(n.getDate() - 1))
-        if (this.statusData[this.getDate(n.getFullYear(), n.getMonth()+1, n.getDate())] != 1) {
-          break
+        key = this.getDate(n.getFullYear(), n.getMonth()+1, n.getDate())
+        if (!(key in this.statusData) || (key in this.statusData && this.statusData[key] != 1)) {
+          success = false
         } else {
           streak++
         }
