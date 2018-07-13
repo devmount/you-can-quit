256c045d619015220e049ff4ffdb1bde814019cc
Andreas Müller <mail@devmount.de>
2018-07-13 17:31:52 +0200

    added: longest streak

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -2,7 +2,8 @@
 <div class="container">
   <h2>Stats</h2>
   <div class="stats">
-    Current streak: {{ currentStreak }}
+    Current streak: {{ currentStreak }}<br>
+    Longest streak: {{ longestStreak }}
   </div>
 </div>
 </template>
@@ -14,24 +15,49 @@ export default {
   },
   methods: {
     // build date format yyyy-mm-dd
-    getDate: function(year, month, day) {
+    getDate (year, month, day) {
       return year + '-' + ('0' + month).slice(-2) + '-' + ('0' + day).slice(-2)
     },
   },
   computed: {
-    // get number of successful days in a row preceding today
-    currentStreak: function() {
-      var streak = 0, n = new Date(), success = true, key = ''
-      while (success) {
+    // get the minimum date (edited date that is most past)
+    minDate () {
+      var keys = Object.keys(this.statusData)
+      if (typeof keys !== 'undefined' && keys.length > 0) {
+          return keys.reduce(function (p, v) {
+            var pd = new Date(p), vd = new Date(v)
+          return ( pd < vd ? pd : vd );
+        });
+      }
+    },
+    // get number of successful days in a row directly preceding today
+    currentStreak () {
+      var streak = 0, n = new Date(), min = this.minDate, key = ''
+      while (min < n) {
         n = new Date(n.setDate(n.getDate() - 1))
         key = this.getDate(n.getFullYear(), n.getMonth()+1, n.getDate())
         if (!(key in this.statusData) || (key in this.statusData && this.statusData[key] != 1)) {
-          success = false
+          break
         } else {
           streak++
         }
       }
       return streak
+    },
+    // get maximum number of successful days in a row
+    longestStreak () {
+      var streak = 0, max = 0, n = new Date(), min = this.minDate, key = ''
+      while (min < n) {
+        n = new Date(n.setDate(n.getDate() - 1))
+        key = this.getDate(n.getFullYear(), n.getMonth()+1, n.getDate())
+        if (!(key in this.statusData) || (key in this.statusData && this.statusData[key] != 1)) {
+          max = streak > max ? streak : max
+          streak = 0
+        } else {
+          streak++
+        }
+      }
+      return max
     }
   }
 }
