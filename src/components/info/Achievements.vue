59bd62d14d5ad0bd36b0e9f00b834ef60d662eaf
devmount <mail@devmount.de>
2018-07-25 00:14:08 +0200

    added: achievement descriptions in tooltip style

--- src/components/info/Achievements.vue
+++ src/components/info/Achievements.vue
@@ -7,129 +7,153 @@
       id="beginning"
       class="item"
       :class="{ active: achievedBeginning }"
-      title="The Beginning"
     >
       <font-awesome-icon icon="sign-out-alt" class="icon" />
-      <div class="description">Mark the first day successful</div>
+      <div class="description">
+        <div class="title">The Beginning</div>
+        Mark the first day successful
+      </div>
     </div>
     <!-- achievement: speed | 7 successful days in a row -->
     <div
       id="speed"
       class="item"
       :class="{ active: achievedSpeed > 0 }"
-      title="Pick Up Speed"
     >
       <div class="badge" v-if="achievedSpeed > 1">{{ achievedSpeed }}</div>
       <font-awesome-icon icon="tachometer-alt" class="icon" />
-      <div class="description">7 successful days in a row</div>
+      <div class="description">
+        <div class="title">Pick Up Speed</div>
+        7 successful days in a row
+      </div>
     </div>
     <!-- achievement: alea | A whole month with 6 fails or less -->
     <div
       id="alea"
       class="item"
       :class="{ active: achievedAlea > 0 }"
-      title="Alea Iacta Est"
     >
       <div class="badge" v-if="achievedAlea > 1">{{ achievedAlea }}</div>
       <font-awesome-icon icon="dice-six" class="icon" />
-      <div class="description">A whole month with 6 fails or less</div>
+      <div class="description">
+        <div class="title">Alea Iacta Est</div>
+        A whole month with 6 fails or less
+      </div>
     </div>
     <!-- achievement: tide | More successful days than failed days -->
     <div
       id="tide"
       class="item"
       :class="{ active: achievedTide }"
-      title="The tide is turned"
     >
       <font-awesome-icon icon="umbrella-beach" class="icon" />
-      <div class="description">More successful days than failed days</div>
+      <div class="description">
+        <div class="title">The tide is turned</div>
+        More successful days than failed days
+      </div>
     </div>
     <!-- achievement: defense | 3 successful days after a one day fail -->
     <div
       id="defense"
       class="item"
       :class="{ active: achievedDefense > 0 }"
-      title="Strong Defense"
     >
       <div class="badge" v-if="achievedDefense > 1">{{ achievedDefense }}</div>
       <font-awesome-icon icon="shield-alt" class="icon" />
-      <div class="description">3 successful days after a one day fail</div>
+      <div class="description">
+        <div class="title">Strong Defense</div>
+        3 successful days after a one day fail
+      </div>
     </div>
     <!-- achievement: praise | 5 successful sundays in a row -->
     <div
       id="praise"
       class="item"
       :class="{ active: achievedPraise > 0 }"
-      title="Praise The Lord"
     >
       <div class="badge" v-if="achievedPraise > 1">{{ achievedPraise }}</div>
       <font-awesome-icon icon="church" class="icon" />
-      <div class="description">5 successful sundays in a row</div>
+      <div class="description">
+        <div class="title">Praise The Lord</div>
+        5 successful sundays in a row
+      </div>
     </div>
     <!-- achievement: uptrend | 4 times more successful days than failed days -->
     <div
       id="uptrend"
       class="item"
       :class="{ active: achievedUptrend }"
-      title="Rising tendency of Success"
     >
       <font-awesome-icon icon="chart-line" class="icon" />
-      <div class="description">4 times more successful days than failed days</div>
+      <div class="description">
+        <div class="title">Rising tendency of Success</div>
+        4 times more successful days than failed days
+      </div>
     </div>
     <!-- achievement: gatherer | Collected 15 achievements (without itself) -->
     <div
       id="gatherer"
       class="item"
       :class="{ active: achievedGatherer > 0 }"
-      title="Gatherer"
     >
       <div class="badge" v-if="achievedGatherer > 1">{{ achievedGatherer }}</div>
       <font-awesome-icon icon="award" class="icon" />
-      <div class="description">Collected 15 achievements (without itself)</div>
+      <div class="description">
+        <div class="title">Gatherer</div>
+        Collected 15 achievements (without itself)
+      </div>
     </div>
     <!-- achievement: clean | A whole month without a fail -->
     <div
       id="clean"
       class="item"
       :class="{ active: achievedClean > 0 }"
-      title="Stay Clean"
     >
       <div class="badge" v-if="achievedClean > 1">{{ achievedClean }}</div>
       <font-awesome-icon icon="broom" class="icon" />
-      <div class="description">A whole month without a fail</div>
+      <div class="description">
+        <div class="title">Stay Clean</div>
+        A whole month without a fail
+      </div>
     </div>
     <!-- achievement: epic | 40 successful days in a row -->
     <div
       id="epic"
       class="item"
       :class="{ active: achievedEpic > 0 }"
-      title="Epic Trophy"
     >
       <div class="badge" v-if="achievedEpic > 1">{{ achievedEpic }}</div>
       <font-awesome-icon icon="trophy" class="icon" />
-      <div class="description">40 successful days in a row</div>
+      <div class="description">
+        <div class="title">Epic Trophy</div>
+        40 successful days in a row
+      </div>
     </div>
     <!-- achievement: master | A total of 365 successful days -->
     <div
       id="master"
       class="item"
       :class="{ active: achievedMaster > 0 }"
-      title="Master Of Success"
     >
       <div class="badge" v-if="achievedMaster > 1">{{ achievedMaster }}</div>
       <font-awesome-icon icon="graduation-cap" class="icon" />
-      <div class="description">A total of 365 successful days</div>
+      <div class="description">
+        <div class="title">Master Of Success</div>
+        A total of 365 successful days
+      </div>
     </div>
     <!-- achievement: legend | A whole year without a fail -->
     <div
       id="legend"
       class="item"
       :class="{ active: achievedLegend > 0 }"
-      title="Legendary Monument"
     >
       <div class="badge" v-if="achievedLegend > 1">{{ achievedLegend }}</div>
       <font-awesome-icon icon="monument" class="icon" />
-      <div class="description">A whole year without a fail</div>
+      <div class="description">
+        <div class="title">Legendary Monument</div>
+        A whole year without a fail
+      </div>
     </div>
     <!-- offset to show all items inline next to each other -->
     <div v-for="i in 3" class="item offset"></div>
@@ -379,6 +403,55 @@ export default {
 
 }
 .achievements .item .description {
-  display: none;
+  position: absolute;
+  font-size: .5em;
+  line-height: 1.1em;
+  visibility: hidden;
+  opacity: 0;
+  z-index: 2;
+  transition: all 0.2s;
+  width: 210px;
+  background: var(--c-background-element);
+  padding: 13px;
+  top: 84px;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0 8px 20px -8px var(--c-shadow);
+}
+.achievements .item .description::before {
+  content: '';
+  position: absolute;
+  top: -14px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 0;
+  height: 0;
+  border-left: 14px solid transparent;
+  border-right: 14px solid transparent;
+  border-bottom: 14px solid var(--c-background-element);
+}
+.achievements .item .description .title {
+  position: relative;
+  font-size: 1.3em;
+  line-height: 1.5em;
+  padding-bottom: 1em;
+}
+.achievements .item .description .title::after {
+  content: '';
+  position: absolute;
+  bottom: .5em;
+  width: 20%;
+  max-width: 120px;
+  height: 2px;
+  background: var(--c-accent);
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0 1px 10px -1px var(--c-shadow);
+}
+.achievements .item:hover .description,
+.achievements .item:focus .description,
+.achievements .item:active .description {
+  visibility: visible;
+  opacity: 1;
 }
 </style>
