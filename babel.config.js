73147e01c644bc288c3952534509d6f7b2f5bc4b
devmount <mail@devmount.de>
2018-08-22 20:27:53 +0200

    added: vue-i18n and vue-i18n-loader

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          preLoaders: {
+            i18n: 'yaml-loader'
+          },
+          loaders: {
+            i18n: '@kazupon/vue-i18n-loader'
+          }
+        }
+      }
+    ]
+  }
 }
