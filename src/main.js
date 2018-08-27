59fccad78f50bbc698f42d6ff36ba6664b7fc185
devmount <mail@devmount.de>
2018-08-27 22:49:59 +0200

    added: language detection

--- src/main.js
+++ src/main.js
@@ -75,7 +75,7 @@ Vue.use(Notifications)
 import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 const i18n = new VueI18n({
-  locale: 'de',
+  locale: navigator.language.substring(0, 2),
   fallbackLocale: 'en',
   messages: {
     "en": require("./locales/en.json"),
