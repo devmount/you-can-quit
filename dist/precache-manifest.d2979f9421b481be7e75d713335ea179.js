2a8cc82aa113e85f91a04fd15fc7437cc5c9e4f0
Andreas Müller <mail@devmount.de>
2020-04-28 22:24:38 +0200

    🔨 finish installation of pwa

---
+++ dist/precache-manifest.d2979f9421b481be7e75d713335ea179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2467d8c20d46935b441c",
+    "url": "css/app.81f31d5c.css"
+  },
+  {
+    "revision": "39635cd190f8971bd01a624a9153a6bc",
+    "url": "img/icon-192.png"
+  },
+  {
+    "revision": "4b6cfd90bb6afa89455d232b0578c69c",
+    "url": "index.html"
+  },
+  {
+    "revision": "2467d8c20d46935b441c",
+    "url": "js/app.a5fb29be.js"
+  },
+  {
+    "revision": "351c32cf5fd2d3efbc4e",
+    "url": "js/chunk-vendors.c94bef4f.js"
+  },
+  {
+    "revision": "6ebc983b2730896c1ed1765e567c6293",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
